11433f8909429aa8d70d2583d8952e16aa160c2e
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-26 12:11:24 -0500

    Add route for links

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import './App.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import store from './Redux/configureStore';
+import { useSelector } from 'react-redux';
 import Nav from './Components/Nav/Nav';
 import Main from './Components/Main/Main';
 import Large from './Components/Dogsize/Large';
 import Medium from './Components/Dogsize/Medium';
 import Small from './Components/Dogsize/Small';
+import Dog from './Components/Dog/Dog';
 
 function App() {
+  const dogs = useSelector((state) => state.dogs);
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Nav>
-          here will go the Nav
-        </Nav>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/Large" element={<Large />} />
-          <Route path="Medium" element={<Medium />} />
-          <Route path="Small" element={<Small />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Large" element={<Large />} />
+        <Route path="/Medium" element={<Medium />} />
+        <Route path="/Small" element={<Small />} />
+        <Route path="/dog/:id" element={<Dog dogs={dogs} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
